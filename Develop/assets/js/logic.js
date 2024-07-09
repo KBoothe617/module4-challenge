b11cd1fafc577263d26924267a78aaceb6e957cc
Kaleb Boothe <kboothe617@gmail.com>
2024-07-08 21:13:32 -0600

    read and write to local storage toggle

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -1,3 +1,10 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
 
 // TODO: Create functions to read and write from local storage
+function readFromLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
+function writeToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
